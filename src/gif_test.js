cce4b3f5c6d847455babf54b65c4b69c9c53e652
Ben Ellis <benjaminster@gmail.com>
2012-08-11 10:59:04 -0700

    Test including image encode/decode

--- src/gif_test.js
+++ src/gif_test.js
@@ -8,24 +8,43 @@ goog.require('goog.testing.stacktrace');
 
 var console = window.console;
 var result = [];
-var test_decode = function() {
-  var restOfTest = function(event) {
+
+var lastGif = null;
+var lastResult = null;
+var testWithGif = function(uriOfGif, functionOfGif) {
+  var decodeAndTest = function() {
     try {
-      var buf = event.target.getResponse();
+      var buf = this.getResponse();
       var reader = new ble.ArrayReader(buf);
       var gif = new ble.Gif();
-      console.log(gif.decode(reader));
+      console.log("Decode success: " + gif.decode(reader).toString());
       console.log(gif);
-      result = gif;
+      lastGif = gif;
+      lastResult = functionOfGif(gif);
     } catch(err) {
       console.log(err.stack);
-      console.log(goog.testing.stacktrace.parse_(err.stack));
+      console.log(goog.testing.stacktrace.parse_(err.stack)); 
     }
   };
-  var io = new goog.net.XhrIo();
-  io.setResponseType(goog.net.XhrIo.ResponseType.ARRAY_BUFFER);
-  goog.events.listen(io, goog.net.EventType.COMPLETE, restOfTest);
-  io.send('../gifs/bwanim.gif', 'GET'); 
+
+  var xhr = new goog.net.XhrIo();
+  xhr.setResponseType(goog.net.XhrIo.ResponseType.ARRAY_BUFFER);
+  goog.events.listen(xhr, goog.net.EventType.COMPLETE, decodeAndTest);
+  xhr.send(uriOfGif, 'GET');
 };
 
-test_decode();
+testWithGif("../gifs/2BC.gif", function(gif){ return testEncode(gif.blocks[1]); });
+
+
+var testEncode = function(block) {
+  var writer = ble.ArrayWriter.ofCapacity(1024*1024);
+  writer = ble.Writer.promote(writer);
+  block.encode(writer);
+  var encoded = writer.writtenSlice();
+  var encodedBuf = encoded.buffer.slice(0, encoded.length);
+  var reader = new ble.ArrayReader(encodedBuf);
+  reader = ble.Reader.promote(reader);
+  var newBlock = new (block.constructor)();
+  newBlock.decode(reader);
+  return {writer: writer, reader: reader, decoded: newBlock};
+};
